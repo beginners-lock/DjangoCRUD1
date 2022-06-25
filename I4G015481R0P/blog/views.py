9192e17202ef3af39401295235314f18a88a027f
beginners-lock <chukwurophi@gmail.com>
2022-06-25 12:05:56 +0100

    first commit

---
+++ I4G015481R0P/blog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from .models import Post
+
+# Create your views here.
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+
